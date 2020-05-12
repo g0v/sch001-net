ce1b2b800fd1fa032fb960091e47d8b0d5d94fe4
SeanGau <rrtw0627@gmail.com>
2020-05-12 12:23:23 +0800

    regen for b80df31

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const pug = require('gulp-pug');
-
-gulp.task('pug', () => {
-  return gulp
-    .src('src/**/*.pug')
-    .pipe(pug())
-    .pipe(gulp.dest('./static/'));
-});
