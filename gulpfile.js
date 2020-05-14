efc533dd276b4eaa3aa53b8c8ccb2e73101dbeb2
SeanGau <rrtw0627@gmail.com>
2020-05-14 13:22:02 +0800

    add more info

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
+const data = require('gulp-data');
 const server = require('gulp-express');
+const fs = require('fs');
 const browserSync = require('browser-sync').create();
 
-gulp.task('server', function () {
-    gulp.src('src/**/*.pug').pipe(pug()).pipe(gulp.dest('./static/'));
-    
+gulp.task('server', function () {    
+    gulp.src('src/**/*.pug').pipe(data(function(file) {
+      return JSON.parse(fs.readFileSync('data/index.json'));
+    })).pipe(pug()).pipe(gulp.dest('./static/'));
     server.run(['app.js']);    
 
-    gulp.watch(['src/**/*.pug'], function(event){
+    gulp.watch(['src/**/*.pug'], function(event){      
       gulp.src('src/**/*.pug').pipe(pug()).pipe(gulp.dest('./static/'));
-      browserSync.reload;
+      browserSync.reload();
+      event();
     });
 });
