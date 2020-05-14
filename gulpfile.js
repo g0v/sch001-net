681c547aa2f4f7f6f30de5d280d5bbf69a097cc8
SeanGau <rrtw0627@gmail.com>
2020-05-14 16:52:36 +0800

    add community partners and some styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,17 @@ const pug = require('gulp-pug');
 const data = require('gulp-data');
 const server = require('gulp-express');
 const fs = require('fs');
-const browserSync = require('browser-sync').create();
 
 gulp.task('server', function () {    
-    gulp.src('src/**/*.pug').pipe(data(function(file) {
+    gulp.src('src/**/index.pug').pipe(data(function(file) {
       return JSON.parse(fs.readFileSync('data/index.json'));
     })).pipe(pug()).pipe(gulp.dest('./static/'));
     server.run(['app.js']);    
 
     gulp.watch(['src/**/*.pug'], function(event){      
-      gulp.src('src/**/*.pug').pipe(pug()).pipe(gulp.dest('./static/'));
-      browserSync.reload();
+      gulp.src('src/**/index.pug').pipe(data(function(file) {
+        return JSON.parse(fs.readFileSync('data/index.json'));
+      })).pipe(pug()).pipe(gulp.dest('./static/'));
       event();
     });
 });
