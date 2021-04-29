f4a79c6ca3d10982f7df3b652db59a15f01a924c
Sean Gau <rrtw0627@gmail.com>
2021-04-29 12:54:35 +0800

    add i18n

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,32 @@
 const gulp = require('gulp');
-const pug = require('gulp-pug');
+const pug = require('gulp-pug-i18n');
 const data = require('gulp-data');
 const connect = require('gulp-connect');
 const sitemap = require('gulp-sitemap');
 const fs = require('fs');
+
+function deploy() {
+  gulp.src('src/**/index.pug')
+  .pipe(data((file) => {
+    console.log("[build] "+file['history']);
+    const result = {
+      index: require('./data/index.json'),
+      index_old: require('./data/index2020.json'),
+      power: require('./data/power.json')
+    };
+    return result;
+  }))
+  .pipe(pug({
+    i18n: {
+      locales: 'locale/*', // locales: en.yml, de.json,
+      filename: '{{basename}}.{{lang}}.html'
+    }
+  }))
+  .pipe(gulp.dest('./static/'))
+  .pipe(sitemap({siteUrl: 'https://sch001.g0v.tw'}))
+  .pipe(gulp.dest('./static/'))
+  .pipe(connect.reload());
+}
 
 gulp.task('server', function () {
     connect.server({
@@ -11,38 +34,10 @@ gulp.task('server', function () {
         livereload: true,
         root: 'static'
     })
-    gulp.src('src/**/index.pug')
-    .pipe(data((file) => {
-      console.log("[build] "+file['history']);
-      const result = {
-        index: require('./data/index.json'),
-        index_old: require('./data/index2020.json'),
-        power: require('./data/power.json')
-      };
-      return result;
-    }))
-    .pipe(pug())
-    .pipe(gulp.dest('./static/'))
-    .pipe(sitemap({siteUrl: 'https://sch001.g0v.tw'}))
-    .pipe(gulp.dest('./static/'))
-    .pipe(connect.reload());
+    deploy();
 
     gulp.watch(['src/**/*.pug', 'data/*.json'], function(event){
-      gulp.src('src/**/index.pug')
-      .pipe(data((file) => {
-        console.log("[build] "+file['history']);
-        const result = {
-          index: require('./data/index.json'),
-          index_old: require('./data/index2020.json'),
-          power: require('./data/power.json')
-        };
-        return result;
-      }))
-      .pipe(pug())
-      .pipe(gulp.dest('./static/'))
-      .pipe(sitemap({siteUrl: 'https://sch001.g0v.tw'}))
-      .pipe(gulp.dest('./static/'))
-      .pipe(connect.reload());
+      deploy();
       event();
     });
 });
