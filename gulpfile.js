7c4c403c395f73055d15a02d4c6863aa3d8c31e0
Sean Gau <rrtw0627@gmail.com>
2021-04-29 16:26:30 +0800

    fix i18n usage

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,43 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug-i18n');
-const data = require('gulp-data');
 const connect = require('gulp-connect');
 const sitemap = require('gulp-sitemap');
 const fs = require('fs');
 
+
+
 function deploy() {
-  gulp.src('src/**/index.pug')
-  .pipe(data((file) => {
-    console.log("[build] "+file['history']);
-    const result = {
-      index: require('./data/index.json'),
-      index_old: require('./data/index2020.json'),
-      power: require('./data/power.json')
-    };
-    return result;
-  }))
-  .pipe(pug({
-    i18n: {
-      locales: 'locale/*', // locales: en.yml, de.json,
-      filename: '{{basename}}.{{lang}}.html'
-    }
-  }))
-  .pipe(gulp.dest('./static/'))
-  .pipe(sitemap({siteUrl: 'https://sch001.g0v.tw'}))
-  .pipe(gulp.dest('./static/'))
-  .pipe(connect.reload());
+  const datas = {
+    index: require('./data/index.json'),
+    index_old: require('./data/index2020.json'),
+    power: require('./data/power.json')
+  };
+  console.log(datas);
+  gulp.src('src/**/index.pug')    
+    .pipe(pug({
+      data: datas,
+      i18n: {
+        default: 'zh-TW',
+        locales: 'src/locale/*.yml', // locales: en.yml, de.json,
+        filename: '{{basename}}{-{{lang}}}{-{{region}}}.html'
+      }
+    }))
+    .pipe(gulp.dest('./static/'))
+    .pipe(sitemap({ siteUrl: 'https://sch001.g0v.tw' }))
+    .pipe(gulp.dest('./static/'))
+    .pipe(connect.reload());
 }
 
 gulp.task('server', function () {
-    connect.server({
-        port: 3000,
-        livereload: true,
-        root: 'static'
-    })
-    deploy();
+  connect.server({
+    port: 3000,
+    livereload: true,
+    root: 'static'
+  })
+  deploy();
 
-    gulp.watch(['src/**/*.pug', 'data/*.json'], function(event){
-      deploy();
-      event();
-    });
+  gulp.watch(['src/**/*.pug', 'data/*.json'], function (event) {
+    deploy();
+    event();
+  });
 });
